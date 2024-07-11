a0a32037c818e024eecee2551da321a099e00917
canoe <760548276@qq.com>
2024-07-11 11:14:21 +0800

    useCallback-forwardRef

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
 import './App.css';
 import React, {
-  useEffect, useLayoutEffect, useState,
+  forwardRef,
+  useCallback,
+  useEffect, useLayoutEffect, useRef, useState,
 } from 'react';
 
-function Bpp() {
-  const [show, setShow] = useState(false);
+const Bpp = forwardRef((prop, ref) => {
+  const { click } = prop;
 
   return (
       <div>
-        { show && <App /> }
-        <button onClick={() => { setShow(!show) }}>bottom</button>
+        <h1 ref={ref} onClick={click}>Bpp</h1>
       </div>
   )
-}
+})
 
 
 function App() {
-  const [name, setName] = useState("canoe");
   const [count, setCount] = useState(0);
+  const bppRef = useRef();
+
+  const click = useCallback(() => {
+    window.alert(1);
+  }, [])
 
   useEffect(() => {
-    console.log("useEffect状态发生变化", count);
-  }, []);
-
-  useLayoutEffect(() => {
-    console.log("useLayoutEffect状态发生变化", count);
-  }, []);
+    console.log(bppRef.current);
+  }, [])
 
   return (
         <div>
           {count}
-          {name}
-          <button onClick={() => { setName("canoe1") }}>name</button>
           <button onClick={() => { setCount((count) => count + 1) }}>count</button>
+          <Bpp ref={bppRef} click={click}/>
         </div>
   )
 }
 
-export default Bpp;
+export default App;
